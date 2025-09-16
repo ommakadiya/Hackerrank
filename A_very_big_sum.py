8b453b786b9aaf192a45e60b02e93781236286fc
ommakadiya_ <154418802+ommakadiya@users.noreply.github.com>
2025-09-16 08:13:47 +0530

    Create A_very_big_sum.py

---
+++ A_very_big_sum.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import os
+import sys
+
+#
+# Complete the 'aVeryBigSum' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER_ARRAY ar as parameter.
+#
+
+def aVeryBigSum(ar):
+    # Just sum all elements in the array
+    longsum = 0
+    for num in ar:
+        longsum += num
+    return longsum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = aVeryBigSum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
